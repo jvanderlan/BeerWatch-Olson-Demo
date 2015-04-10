adf67b94e7c302a9f78a03ff1ab6d9062e726903
jvanderlan <jvanderlan@icfi.com>
2015-04-10 14:24:19 -0500

    Initial Commit

---
+++ Beer Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Beer Watch
+//
+//  Created by Vanderlan, Jeremiah on 4/10/15.
+//  Copyright (c) 2015 Vanderlan, Jeremiah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
